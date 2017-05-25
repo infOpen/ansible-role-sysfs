7813620ce4b5bd5bcb43385e39182294d94169d8
Alexandre Chaussier <a.chaussier@infopen.pro>
2017-05-25 15:05:07 +0200

    Add service management

--- tests/test_configuration.py
+++ tests/test_configuration.py
@@ -26,6 +26,34 @@ def test_rules_folder(host):
     assert host.file(rules_folder['path']).mode == rules_folder['mode']
 
 
+def test_rules_files(host):
+    """
+    Test if rules files exists
+    """
+
+    rules_file = {
+        'path': '/etc/sysfs.d/transparent-huge-pages.conf',
+        'user': 'root',
+        'group': 'root',
+        'mode': 0o400,
+    }
+
+    expected_content = {
+        'kernel/mm/transparent_hugepage/defrag = never',
+        'kernel/mm/transparent_hugepage/enabled = never',
+    }
+
+    file_content = host.check_output('cat {}'.format(rules_file['path']))
+    file_content_set = set(it.strip() for it in file_content.split("\n"))
+
+    assert host.file(rules_file['path']).exists
+    assert host.file(rules_file['path']).is_file
+    assert host.file(rules_file['path']).user == rules_file['user']
+    assert host.file(rules_file['path']).group == rules_file['group']
+    assert host.file(rules_file['path']).mode == rules_file['mode']
+    assert expected_content.issubset(file_content_set)
+
+
 def test_sysfs_rules(host):
     """
     Check sysfs rules
